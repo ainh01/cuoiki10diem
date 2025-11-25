7ec01fa61558dbfe84eaa48940b5e692040b1394
ainh01 <128797227+ainh01@users.noreply.github.com>
2025-11-25 20:27:28 +0700

    Update index.vue

--- Services/ui_main/pages/index.vue
+++ Services/ui_main/pages/index.vue
@@ -2,7 +2,7 @@
 import { meta } from '@/utils/data';
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_OTRUYEN_API_BASE_URL;
   
 const MyAPI = axios.create({
   baseURL: API_BASE_URL,
